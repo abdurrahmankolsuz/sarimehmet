6c3c3561b6f252e704cba0c7936fb07e85ed40a0
Abdurrahman KOLSUZ <kolsuzabdurrahman@gmail.com>
2016-12-20 01:36:07 +0300

    Revert "Delete psl-config.php"
    This reverts commit 69ac8e0475639e277d7becd3d122c6b83bd888b4.

---
+++ psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "root");    // The database username.  //sarimehm_sari
+define("PASSWORD", "");    // The database password. //apo2036987
+define("DATABASE", "mydb");    // The database name. //sarimehm_mydb
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
